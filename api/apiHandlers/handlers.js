df81258d87ebf966da8f5cc0e96afff6bd6374cb
Viktor Bezruk <viktor.bezruk@devico.io>
2021-02-02 19:29:13 +0200

    fix: fix handlers

--- api/apiHandlers/handlers.js
+++ api/apiHandlers/handlers.js
@@ -1,98 +1,93 @@
-import {ObjectId} from 'mongodb'
+import { ObjectId } from 'mongodb'
 
-export const HANDLERS = {
+export const getList = (collection) => {
+  return async ctx => {
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
+}
 
-  getList: function (collection) {
-    return async ctx => {
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
-    }
-  },
+export const addItem = (collection) => {
+  return async ctx => {
+    const body = ctx.request.body;
+    const newItem = {
+      task: body.task,
+      isActive: true,
+    };
+    await collection.insertOne(newItem);
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
+}
 
-  addItem: function (collection) {
-    return async ctx => {
-      const body = ctx.request.body;
-      const newItem = {
-        task: body.task,
-        isActive: true,
-      };
-      await collection.insertOne(newItem);
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
-    }
-  },
+export const removeItem = (collection) => {
+  return async ctx => {
+    const id = ObjectId(ctx.request.body.id);
+    await collection.deleteOne({
+      _id: id
+    })
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
+}
 
-  removeItem: function (collection) {
-    return async ctx => {
-      const id = ObjectId(ctx.request.body.id);
-      await collection.deleteOne({
-        _id: id
-      })
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
-    }
-  },
+export const toggleAllItems = (collection) => {
+  return async ctx => {
+    const isActive = ctx.request.body.isActive;
+    await collection.updateMany({}, {
+      $set: {
+        isActive
+      }
+    })
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
+}
 
-  toggleAllItems: function (collection) {
-    return async ctx => {
-      const isActive = ctx.request.body.isActive;
-      await collection.updateMany({}, {
-        $set: {
-          isActive
-        }
-      })
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
-    }
-  },
+export const toggleItem = (collection) => {
+  return async ctx => {
+    const id = ObjectId(ctx.request.body.id);
+    const currentItem = await collection.findOne({ _id: id });
+    await collection.findOneAndUpdate({
+      _id: id
+    }, {
+      $set: {
+        isActive: !currentItem.isActive
+      }
+    })
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
+}
 
-  toggleItem: function (collection) {
-    return async ctx => {
-      const id = ObjectId(ctx.request.body.id);
-      const currentItem = await collection.findOne({ _id: id });
+export const editItem = (collection) => {
+  return async ctx => {
+    const id = ObjectId(ctx.request.body.id);
+    const content = ctx.request.body.task;
+    if (content.trim().length) {
       await collection.findOneAndUpdate({
         _id: id
       }, {
         $set: {
-          isActive: !currentItem.isActive
+          task: content.trim()
         }
       })
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
-    }
-  },
-
-  editItem: function (collection) {
-    return async ctx => {
-      const id = ObjectId(ctx.request.body.id);
-      const content = ctx.request.body.task;
-      if (content.trim().length) {
-        await collection.findOneAndUpdate({
-          _id: id
-        }, {
-          $set: {
-            task: content
-          }
-        })
-      } else {
-        await collection.deleteOne({
-          _id: id 
-        })
-      }
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
-    }
-  },
-
-  removeCompletedItems: function (collection) {
-    return async ctx => {
-      await collection.deleteMany({
-        isActive: false
+    } else {
+      await collection.deleteOne({
+        _id: id
       })
-      const todoList = await collection.find().toArray();
-      ctx.body = { list: todoList }
     }
-  },
-
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
 }
 
+export const removeCompletedItems = (collection) => {
+  return async ctx => {
+    await collection.deleteMany({
+      isActive: false
+    })
+    const todoList = await collection.find().toArray();
+    ctx.body = { list: todoList }
+  }
+}
--- api/server.js
+++ api/server.js
@@ -4,7 +4,15 @@ import cors from '@koa/cors'
 import bodyParser from 'koa-bodyparser'
 import { MongoClient } from 'mongodb'
 import { ENDPOINTS } from './constants/endpoints'
-import { HANDLERS } from './apiHandlers/handlers'
+import {
+  getList,
+  addItem,
+  removeItem,
+  toggleAllItems,
+  toggleItem,
+  editItem,
+  removeCompletedItems
+} from './apiHandlers/handlers'
 import { config } from './config'
 
 const uri = config.DB_URL
@@ -16,16 +24,16 @@ server.use(bodyParser());
 
 (async () => {
   const client = await MongoClient.connect(uri, { useUnifiedTopology: true });
-  const db = client.db('todoDB');
-  const todosCollection = db.collection('todos');
+  const db = client.db(process.env.MONGO_DB_NAME);
+  const todosCollection = db.collection(process.env.MONGO_DB_COLLECTION);
 
-  router.get(ENDPOINTS.home, HANDLERS.getList(todosCollection))
-  router.post(ENDPOINTS.addItem, HANDLERS.addItem(todosCollection))
-  router.post(ENDPOINTS.editItem, HANDLERS.editItem(todosCollection))
-  router.post(ENDPOINTS.removeItem, HANDLERS.removeItem(todosCollection))
-  router.post(ENDPOINTS.toggleItem, HANDLERS.toggleItem(todosCollection))
-  router.post(ENDPOINTS.toggleAllItems, HANDLERS.toggleAllItems(todosCollection))
-  router.post(ENDPOINTS.removeCompletedItems, HANDLERS.removeCompletedItems(todosCollection))
+  router.get(ENDPOINTS.home, getList(todosCollection))
+  router.post(ENDPOINTS.addItem, addItem(todosCollection))
+  router.post(ENDPOINTS.editItem, editItem(todosCollection))
+  router.post(ENDPOINTS.removeItem, removeItem(todosCollection))
+  router.post(ENDPOINTS.toggleItem, toggleItem(todosCollection))
+  router.post(ENDPOINTS.toggleAllItems, toggleAllItems(todosCollection))
+  router.post(ENDPOINTS.removeCompletedItems, removeCompletedItems(todosCollection))
 })()
 
-server.use(router.routes()).listen(3001)
+server.use(router.routes()).listen(process.env.DB_PORT)
