ce540127083a0f8b8ced09d4fa6891471d6d98af
Viktor Bezruk <44390837+bezrukviktor@users.noreply.github.com>
2021-02-03 17:48:15 +0200

    Delete api directory

--- api/config.js
+++
@@ -1,5 +0,0 @@
-import {} from 'dotenv/config'
-
-export const config = {
-  DB_URL: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.a31qk.mongodb.net/tododb?retryWrites=true&w=majority`
-}
